d3781f412d08c4ab4141596d3b54b82f42489b04
rachyharkov <44340813+rachyharkov@users.noreply.github.com>
2021-03-09 15:00:41 +0700

    fix here and there

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,60 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-var i = this.loopNum % this.toRotate.length;
-var fullTxt = this.toRotate[i];
-
-if (this.isDeleting) {
-this.txt = fullTxt.substring(0, this.txt.length - 1);
-} else {
-this.txt = fullTxt.substring(0, this.txt.length + 1);
-}
-
-this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-var that = this;
-var delta = 200 - Math.random() * 100;
-
-if (this.isDeleting) { delta /= 2; }
-
-if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
-} else if (this.isDeleting && this.txt === '') {
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
   this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
-}
-
-setTimeout(function() {
-  that.tick();
-}, delta);
 };
 
-window.onload = function() {
-var elements = document.getElementsByClassName('typewrite');
-for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-type');
-    var period = elements[i].getAttribute('data-period');
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
     if (toRotate) {
       new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-}
-// INJECT CSS
-var css = document.createElement("style");
-css.type = "text/css";
-css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-document.body.appendChild(css);
-
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
 
 var acc = document.getElementsByClassName("accordion-faq");
@@ -62,26 +63,25 @@ var accwidp = document.getElementsByClassName("panel");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-acc[i].addEventListener("click", function() {
-var panel = this.nextElementSibling;
-if (panel.style.maxHeight) {
-  panel.style.maxHeight = null;
-  for(var o=0;o<accwiduwu.length;o++){
-    setHeightofPanel(accwiduwu[o],acc[o]);
-  }
-} else {
-
-  //doing things below because classname is an array (much of element with this classname)
-  for(var x=0;x<accwiduwu.length;x++){
-    setHeightofPanel(accwiduwu[x],acc[x]);
-  }
-  this.classList.add("selected");
-  panel.style.maxHeight = panel.scrollHeight + "px";
+  acc[i].addEventListener("click", function () {
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      for (var o = 0; o < accwiduwu.length; o++) {
+        setHeightofPanel(accwiduwu[o], acc[o]);
+      }
+    } else {
+      //doing things below because classname is an array (much of element with this classname)
+      for (var x = 0; x < accwiduwu.length; x++) {
+        setHeightofPanel(accwiduwu[x], acc[x]);
+      }
+      this.classList.add("selected");
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
-});
-}
 
-function setHeightofPanel(div1,div2) {
-    div1.style.maxHeight = null;
-    div2.classList.remove("selected");
+function setHeightofPanel(div1, div2) {
+  div1.style.maxHeight = null;
+  div2.classList.remove("selected");
 }
